c93f0ffee7d19373521967f1bb663dfcf7633807
USER01\kanno <kanno062316@icloud.com>
2024-06-28 11:34:01 +0900

    コントローラーをDBに接続（途中）

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,8 +7,13 @@ use Illuminate\Http\Request;
 class PostsController extends Controller
 {
     //show($id)ではなくinvoceを使うとスッキリする
-    public function __invoke{$id} {
-        $today = date('Y-m-d')
+    public function __invoke($id) {
+        $today = date('Y-m-d');
+
+        $posts = DB::select('select * from posts.posts where ID =1');
+        $post = $posts[0];
+        var_dump($post);
+
         return view('posts.post', ['id'=>$id, 'today'=>$today]);
     }
 }
